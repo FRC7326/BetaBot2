0d0e17fd22e2c89964404127c5de9d1d6c8916d8
FRC7326 <58712519+FRC7326@users.noreply.github.com>
2019-12-24 13:43:15 -0800

    Chad

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -1,4 +1,5 @@
 package frc.robot;
+import edu.wpi.first.wpilibj.Compressor;
 import edu.wpi.first.wpilibj.TimedRobot;
 import edu.wpi.first.wpilibj.command.Scheduler;
 import frc.robot.subsystems.DriveSubsystem;
@@ -6,9 +7,12 @@ import frc.robot.subsystems.DriveSubsystem;
 public class Robot extends TimedRobot {
   public static DriveSubsystem driveSubsystem = new DriveSubsystem();
   public static OI oi;
+  Compressor c0 = new Compressor(0);
   
   @Override
-  public void robotInit() { oi = new OI(); }
+  public void robotInit() { oi = new OI();
+  c0.setClosedLoopControl(true);
+  }
   @Override
   public void robotPeriodic() {}
   @Override
@@ -22,7 +26,9 @@ public class Robot extends TimedRobot {
   @Override
   public void teleopInit() {}
   @Override
-  public void teleopPeriodic() { Scheduler.getInstance().run(); }
+  public void teleopPeriodic() { Scheduler.getInstance().run(); 
+  if(oi.)
+  }
   @Override
   public void testPeriodic() { }
 }
